04b7596d079688e62f406bc81e1aa99f59b328b3
Swaraj Swain <swaraj.swain@geniobits.in>
2023-02-26 20:12:01 +0530

    FPS

---
+++ Assets/Scripts/Launcher.cs
@@ -0,0 +1,138 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using TMPro;
+using Photon.Realtime;
+using System.Linq;
+
+public class Launcher : MonoBehaviourPunCallbacks
+{
+	public static Launcher Instance;
+
+	[SerializeField] TMP_InputField roomNameInputField;
+	[SerializeField] TMP_Text errorText;
+	[SerializeField] TMP_Text roomNameText;
+	[SerializeField] Transform roomListContent;
+	[SerializeField] GameObject roomListItemPrefab;
+	[SerializeField] Transform playerListContent;
+	[SerializeField] GameObject PlayerListItemPrefab;
+	[SerializeField] GameObject startGameButton;
+
+	void Awake()
+	{
+		Instance = this;
+	}
+
+	void Start()
+	{
+		
+		PhotonNetwork.ConnectUsingSettings();
+		Debug.Log("Connecting to Master");
+	}
+
+	public override void OnConnectedToMaster()
+	{
+		
+		PhotonNetwork.JoinLobby();
+		PhotonNetwork.AutomaticallySyncScene = true;
+		Debug.Log("Connected to Master");
+	}
+
+	public override void OnJoinedLobby()
+	{
+		MenuManager.Instance.OpenMenu("title");
+		Debug.Log("Joined Lobby");
+	}
+
+	public void CreateRoom()
+	{
+		if(string.IsNullOrEmpty(roomNameInputField.text))
+		{
+			return;
+		}
+		PhotonNetwork.CreateRoom(roomNameInputField.text);
+		MenuManager.Instance.OpenMenu("loading");
+	}
+
+	public override void OnJoinedRoom()
+	{
+		MenuManager.Instance.OpenMenu("room");
+		roomNameText.text = PhotonNetwork.CurrentRoom.Name;
+
+		Player[] players = PhotonNetwork.PlayerList;
+
+		//now when we join a room is just gonna delete all the previous player list item and it'll instantiate nthe new ones
+		foreach(Transform child in playerListContent)
+		{
+			Destroy(child.gameObject);
+		}
+
+		for(int i = 0; i < players.Count(); i++)
+		{
+			Instantiate(PlayerListItemPrefab, playerListContent).GetComponent<PlayerListItem>().SetUp(players[i]);
+		}
+
+		//if we are the host our game , when we joined room, the start game button will be set active
+		startGameButton.SetActive(PhotonNetwork.IsMasterClient);
+	}
+
+	public override void OnMasterClientSwitched(Player newMasterClient)
+	{
+		startGameButton.SetActive(PhotonNetwork.IsMasterClient);
+	}
+
+	public override void OnCreateRoomFailed(short returnCode, string message)
+	{
+		errorText.text = "Room Creation Failed: " + message;
+		Debug.LogError("Room Creation Failed: " + message);
+		MenuManager.Instance.OpenMenu("error");
+	}
+
+	public void StartGame()
+	{
+		PhotonNetwork.LoadLevel(1);
+	}
+
+	public void LeaveRoom()
+	{
+		PhotonNetwork.LeaveRoom();
+		MenuManager.Instance.OpenMenu("loading");
+	}
+
+	public void JoinRoom(RoomInfo info)
+	{
+		PhotonNetwork.JoinRoom(info.Name);
+		MenuManager.Instance.OpenMenu("loading");
+	}
+
+	public override void OnLeftRoom()
+	{
+		MenuManager.Instance.OpenMenu("title");
+	}
+
+	public override void OnRoomListUpdate(List<RoomInfo> roomList)
+	{
+		foreach(Transform trans in roomListContent)
+		{
+			Destroy(trans.gameObject);
+		}
+
+		for(int i = 0; i < roomList.Count; i++)
+		{
+			//when we're looping through a room list here , just gonna check 
+			if (roomList[i].RemovedFromList)
+				//this means we just skip this(Instantiate) and continue on to the next item in the loop coz as it turn out in photons room list , it doesn't remove rooms from the list that have been removed from the list it actally just set that bool(RemovedFromList) to true when they are removed from the list
+                //so we just have to check that and if that room has been removed from the list then we can just not instantiate it again
+				continue;                                                              
+			Instantiate(roomListItemPrefab, roomListContent).GetComponent<RoomListItem>().SetUp(roomList[i]);
+		}
+
+		
+	}
+
+	public override void OnPlayerEnteredRoom(Player newPlayer)
+	{
+		Instantiate(PlayerListItemPrefab, playerListContent).GetComponent<PlayerListItem>().SetUp(newPlayer);
+	}
+}
